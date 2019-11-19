0323e37ad0da58721d188c05ff2eca665948fbdc
Scott Sauceda <msauceda0812@gmail.com>
2019-11-18 20:59:33 -0500

    first commit

---
+++ src/main/java/co/ga/bookstore/BookRepository.java
@@ -0,0 +1,21 @@
+package co.ga.bookstore;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import io.swagger.annotations.ApiOperation;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    @RestResource(path = "byTitle")
+    @ApiOperation("find all books that contain the string q in their title, ignoring case")
+    List<Book> findByTitleIgnoreCaseContaining(@Param("q") String q);
+
+
+
+}
+
